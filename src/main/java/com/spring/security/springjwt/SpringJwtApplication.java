60c05c4cc6249ac6b9b4f87396f5d65034af3233
Naveen Kumar Sathar <naveensathar431@gmail.com>
2022-01-10 23:46:45 +0530

    Initial Commit

---
+++ src/main/java/com/spring/security/springjwt/SpringJwtApplication.java
@@ -0,0 +1,16 @@
+package com.spring.security.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+
+}
+
+
+//eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmb28iLCJleHAiOjE2Mjg5NzMyNDQsImlhdCI6MTYyODkzNzI0NH0.gy5Dg7lbs5bHbvTVFfAkOcs_pt98ms6xl0DbV14I9T4
