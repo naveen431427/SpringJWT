5dce4143f186408a72fc82b78c7d7d935209d6dd
Naveen Kumar Sathar <naveensathar431@gmail.com>
2022-01-10 23:48:11 +0530

    Initial Commit

--- src/main/java/com/spring/security/springjwt/filters/JwtRequestFilter.java
+++ src/main/java/com/spring/security/springjwt/filters/JwtRequestFilter.java
@@ -38,6 +38,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             username = jwtUtil.extractUsername(jwt);
+
         }
 
 
